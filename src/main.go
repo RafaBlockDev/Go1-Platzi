bf311c3a4d0afd512d3c55938245fac28f26d0a3
RafaelCherreraco <rafael@cherrera.co>
2021-08-24 13:44:00 -0500

    Commit v1.0

---
+++ src/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	// For conditional
+
+	for i := 0; i <= 10; i++{
+		// i empieza con 0 y se ejecuta siempre
+		// y cuando sea menor a diez,
+		// incrementando 1
+		fmt.Println(i)
+	}
+
+	fmt.Println("\n") // \n = espaciado
+
+	// For While
+	counter := 0
+	for counter <= 10 {
+		fmt.Println(counter)
+		counter++ // Incremental del 0 => 10
+	}
+
+	// For forever
+	counterForever := 0
+	for {
+		// Se ejecutara por siempre porque no hay condiciones
+		fmt.Println(counterForever)
+		counterForever++
+	}
+}
